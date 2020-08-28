f16efbe56ed7547a9f3cc23b18a5774955c20c2c
Gabyblackroses <mgabypaz@hotmail.com>
2020-08-28 02:11:48 -0300

    final

--- formulario.js
+++ formulario.js
@@ -5,19 +5,20 @@ function validar(){
     var nombrexp = /^[a-z ,.'-]+$/i;
     var apellidos = document.getElementById("apellidos").value;
     var edad = document.getElementById("edad").value;
-    var edadexp = /^[0-9]$/;
+    var edadexp = /^[0-9]{1,3}$/;  
     var correo = document.getElementById("correo").value;
     var correoexp =/^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;  
     var especialidad = document.getElementById("especialidad").value;
     var fecha = document.getElementById("fecha").value;
-    var fechaexp = /^\d{2}[.-/]\d{2}[.-/]\d{4}$/; 
+    var fechaexp = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
     var hora = document.getElementById("hora").value;
 
+    
     if (rut === ""){
         alert("Favor ingrese su rut");
         return false;
     }
-    else if (!rut.test(rutexp)){
+    else if (!rutexp.test(rut)){
         alert("El rut es erroneo");
         return false;
     } 
@@ -25,7 +26,7 @@ function validar(){
         alert("Todos los campos son obligatorios");
         return false;
     }
-    else if (!nombre.test(nombrexp)){
+    else if (!nombrexp.test(nombre)){
         alert("El nombre no valido");
         return false;
     }
@@ -41,11 +42,7 @@ function validar(){
         alert("Todos los campos son obligatorios");
         return false;
     }
-    else if (edad.length>2){
-        alert("La edad es muy larga");
-        return false;
-    }
-    else if (!edad.test(edadexp)){
+    else if (!edadexp.test(edad)){
         alert("La edad no es válida");
             return false;
     }
@@ -57,7 +54,7 @@ function validar(){
         alert("El correo es muy largo");
         return false;
     }
-    else if( (!correo.test(correoexp)) ) {
+    else if( (!correoexp.test(correo)) ) {
         alert("Debe poseer un ‘@’ y un punto ‘.’ ");
         return false;
     }
@@ -65,13 +62,14 @@ function validar(){
         alert("Todos los campos son obligatorios");
         return false;
     } 
-    else if( (!fecha.test(fechaexp)) ) {
+    else if( (!fechaexp.test(fecha)) ) {
         alert("Formato dd-mm-yyyy");
         return false;
     }
 
-    if (nombre != "" && apellidos){
-        alert("Estimado(a)"+nombre+ "su hora para ha sido reservada para el día a las. Además, se le envió un mensaje a su correo con el detalle de su cita. Gracias por preferirnos.")
+    if (nombre != "" && apellidos!= "" && fecha!= "" && correo!= ""){
+        alert("Estimado(a) "+nombre+" "+apellidos+", su hora para "+especialidad+" ha sido reservada para el día "+fecha+" a las "+hora+"hrs. Además, se le envió un mensaje a su correo "+correo+" con el detalle de su cita. Gracias por preferirnos.");
+        document.getElementById("resultado").innerHTML = "Estimado(a) "+nombre+" "+apellidos+", su hora para "+especialidad+" ha sido reservada para el día "+fecha+" a las "+hora+"hrs. Además, se le envió un mensaje a su correo "+correo+" con el detalle de su cita. Gracias por preferirnos.";
     }
+    return false;
 }
- //"+apellidos+" "+especialidad+" "+fecha+" "+hora+" "+correo+"  && (apellidos != "") && (fecha != "") && (correo != "")
